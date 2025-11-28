9e89876ecd40e89d22ced78ef0afb8af31f2338e
Minji Kim <sml07113@gmail.com>
2025-11-28 18:09:27 +0900

    feat: 9강 - 오류 문구 노출

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,9 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PostController {
     private final PostService postService;
 
-    @GetMapping("/posts/write")
-    @ResponseBody
-    public String write() {
+    private String getWriteFormHtml() {
         return """
                 <form action="doWrite" method="POST">
                   <input type="text" name="title" placeholder="제목">
@@ -27,9 +26,28 @@ public class PostController {
                 """;
     }
 
+    private String getErrorMessageHtml(String errorMessage) {
+        return """
+                <div style="color:red;">%s</div>
+                """.formatted(errorMessage);
+    }
+
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String write() {
+        return getWriteFormHtml();
+    }
+
     @PostMapping("/posts/doWrite")
     @ResponseBody
-    public String createPost(String title, String content) {
+    public String createPost(@RequestParam(defaultValue = "") String title, @RequestParam(defaultValue = "") String content) {
+        if (title.isBlank()) {
+            return getErrorMessageHtml("제목을 입력해주세요") + getWriteFormHtml();
+        }
+        if (content.isBlank()) {
+            return getErrorMessageHtml("내용을 입력해주세요") + getWriteFormHtml();
+
+        }
         Post newPost = postService.write(title, content);
         return "%d번 글 생성 완료".formatted(newPost.getId());
     }
