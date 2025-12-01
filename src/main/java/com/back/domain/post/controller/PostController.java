7803a6e136527bf84388cd77eff83ada681a149a
Minji Kim <sml07113@gmail.com>
2025-12-01 22:41:59 +0900

    refactor: 24강 - th:with로 변수화

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -29,12 +29,12 @@ public class PostController {
     @AllArgsConstructor
     @Getter
     public static class WriteForm {
-        @NotBlank(message = "1-제목을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "2-제목은 2자 이상, 20자 이하로 입력 가능합니다.")
+        @NotBlank(message = "1-title-제목을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "2-title-제목은 2자 이상, 20자 이하로 입력 가능합니다.")
         private String title;
 
-        @NotBlank(message = "3-내용을 입력해주세요.")
-        @Size(min = 2, max = 100, message = "4-내용은 2자 이상, 20자 이하로 입력 가능합니다.")
+        @NotBlank(message = "3-content-내용을 입력해주세요.")
+        @Size(min = 2, max = 100, message = "4-content-내용은 2자 이상, 20자 이하로 입력 가능합니다.")
         private String content;
     }
 
