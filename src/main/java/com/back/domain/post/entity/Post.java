3b220fae3ce9908beb0cb62f937730376926476f
Minji Kim <sml07113@gmail.com>
2025-12-03 12:12:55 +0900

    feat: 33강 - 댓글 엔티티 및 샘플 데이터 생성

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -1,21 +1,42 @@
 package com.back.domain.post.entity;
 
+import com.back.domain.comment.entity.Comment;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REMOVE;
+
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 public class Post extends BaseEntity {
     private String title;
     private String content;
 
+    // mappedBy: comment에서 저장된 변수명, fetch: 기본값 LAZY
+    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = {PERSIST, REMOVE})
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
     public void modify(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public void addComment(String content) {
+        Comment comment = new Comment(this, content);
+        comments.add(comment);
     }
 }
